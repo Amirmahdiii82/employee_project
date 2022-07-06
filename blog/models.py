45920d91bd045ea9cff7434e2f41bfc733145e9a
amirmahdi <amirmahdiaramideh@gmail.com>
2022-07-06 13:44:22 +0430

    project added

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+
+class Employee(models.Model):
+    # id, name, email, job, salay
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=200)
+    email = models.EmailField(max_length=250)
+    job = models.CharField(max_length=250)
+    salary = models.IntegerField(null=True)
+  
